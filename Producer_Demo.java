21408a3989461996ce83e1c06e7fe12fa306d380
pcbzmani <pcbzmani@gmail.com>
2020-07-12 14:58:14 +0530

    Create Producer_Demo.java
    Producer Demo Java Code, with Maven Plugins

---
+++ Producer_Demo.java
@@ -0,0 +1,36 @@
+package com.github.pcbzmani;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String bootstrapservers = "127.0.0.1:9092";
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapservers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        //create producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic","hWorilello d");
+
+
+        //send messages
+        producer.send(record);
+
+        //flush
+        producer.flush();
+        //flush and close
+        producer.close();
+
+    }
+}
